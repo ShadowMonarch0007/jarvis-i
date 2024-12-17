0a11e2826f0739c97fc3465903d45158e819a06b
Aditya Chandra <aditya.anil.chandra@gmail.com>
2024-12-18 02:27:57 +0530

    bug fix

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://jarvis-i.vercel.app/api/v1/post', {
+      const response = await fetch('https://jarvis-i.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
