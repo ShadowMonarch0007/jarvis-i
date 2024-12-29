85d468d39d0a1991a8975ed2140a8f1e7244fd79
Aditya Chandra <aditya.anil.chandra@gmail.com>
2024-12-29 23:57:53 +0530

    logo added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
-import { logo } from './assets';
+import { logo } from '../public/jarvis.png';
 import { Home, CreatePost } from './page';
 
 const App = () => {
@@ -8,7 +8,10 @@ const App = () => {
     <BrowserRouter>
       <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
         <Link to="/">
-          <img src={logo} alt="logo" className="w-28 object-contain" />
+          <div className="flex items-center gap-2">
+            <Image className="h-16 w-16" src={logo} />
+            <p className="font-bold text-3xl font-code">JARVIS</p>
+          </div>
         </Link>
 
         <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Create</Link>
